e6b6363a4bd86cc8941b5349134b203754071e3e
anyjiujitsu <anynejiujitsu@gmail.com>
2026-01-28 13:44:37 -0500

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,7 @@
-import { loadCSV, normalizeDirectoryRow, normalizeEventRow } from "./data.js?v=20260128-001";
-import { state, setView, setIndexQuery, setEventsQuery } from "./state.js?v=20260128-001";
-import { filterDirectory, filterEvents } from "./filters.js?v=20260128-001";
-import { renderDirectoryGroups, renderEventsGroups } from "./render.js?v=20260128-001";
+import { loadCSV, normalizeDirectoryRow, normalizeEventRow } from "./data.js?v=20260123-016";
+import { state, setView, setIndexQuery, setEventsQuery } from "./state.js?v=20260123-016";
+import { filterDirectory, filterEvents } from "./filters.js?v=20260123-016";
+import { renderDirectoryGroups, renderEventsGroups } P260128-NEW1";
 
 let directoryRows = [];
 let eventRows = [];
